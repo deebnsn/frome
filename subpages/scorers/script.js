aed3001473e10d379443b88e0ee26ea51329de0e
deebnsn <dbenson27@gmail.com>
2017-05-03 17:47:33 +0100

    minorChange

--- subpages/scorers/script.js
+++ subpages/scorers/script.js
@@ -165,8 +165,7 @@ function is_touch_device() {
 }
 
 if (is_touch_device()) {
-d3.select("body").style("height","100%")
-	d3.select("#vis").style("height","100%")
+
  }
  else{
  	window.onresize = function() {
